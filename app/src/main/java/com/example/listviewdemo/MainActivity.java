17b905900dba7050361a4c97ec4541b6ddca7824
Kenia Rioja-Naranjo <kdrioja@gmail.com>
2019-10-20 21:24:52 -0700

    Logs array item that was clicked

--- app/src/main/java/com/example/listviewdemo/MainActivity.java
+++ app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -16,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         ListView myListView = findViewById(R.id.myListView);
 
-        ArrayList<String> myFamily = new ArrayList<>();
+        final ArrayList<String> myFamily = new ArrayList<>();
 
         myFamily.add("Dani");
         myFamily.add("Dennis");
@@ -26,6 +30,22 @@ public class MainActivity extends AppCompatActivity {
         myFamily.add("Siani");
         myFamily.add("Kiki");
 
-        
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamily);
+
+        // now the listView knows what adapter to use
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            // a listener to see which item in the listView is clicked
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // the ? means that there are different types of Collections that can gbe used
+                //parent.setVisibility(View.GONE); to make it disappaer
+                // view is the individual item that has been clicked on
+                // aparent is the entire listView
+                //counts items starting form 0 and given in position
+                Log.i("Name of member", myFamily.get(position));
+            }
+        });
     }
 }
