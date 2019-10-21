f5d975fa3b0a89cc25f7e448e99ff2380bcc4295
Kenia Rioja-Naranjo <kdrioja@gmail.com>
2019-10-20 21:15:52 -0700

    Building array to populate listView

--- app/src/main/java/com/example/listviewdemo/MainActivity.java
+++ app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.listviewdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView myListView = findViewById(R.id.myListView);
+
+        ArrayList<String> myFamily = new ArrayList<>();
+
+        myFamily.add("Dani");
+        myFamily.add("Dennis");
+        myFamily.add("Kida");
+        myFamily.add("Nala");
+        myFamily.add("Mom");
+        myFamily.add("Siani");
+        myFamily.add("Kiki");
+
+        
     }
 }
